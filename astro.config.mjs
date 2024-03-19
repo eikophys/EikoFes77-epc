05821ecde0e20d51df81893d40d1569e92c720c5
SAS NEWS <109570410+sas-news@users.noreply.github.com>
2024-03-19 18:20:39 +0900

    setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import compress from "astro-compress";
+
+import critters from "astro-critters";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [compress(), critters()]
+});
