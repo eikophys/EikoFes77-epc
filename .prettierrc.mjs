8f805579d5f72cb1a08a1856ec693ed0e978fb42
SAS NEWS <109570410+sas-news@users.noreply.github.com>
2024-03-20 18:46:41 +0900

    初期設定

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,17 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  printWidth: 80,
+  quoteProps: "as-needed",
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+  embeddedLanguageFormatting: "auto",
   plugins: ["prettier-plugin-astro"],
   overrides: [
     {
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
-
 import critters from "astro-critters";
+
+import relativeLinks from "astro-relative-links";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), critters()]
+  integrations: [compress(), critters(), relativeLinks()]
 });
