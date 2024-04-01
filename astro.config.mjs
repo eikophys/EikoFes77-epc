3c001d85ce59093a829e95fbf066a309b352c607
SAS NEWS <109570410+sas-news@users.noreply.github.com>
2024-04-01 17:27:07 +0900

    最低限デザイン

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
 import critters from "astro-critters";
-
 import relativeLinks from "astro-relative-links";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), critters(), relativeLinks()]
+  integrations: [compress(), critters(), relativeLinks(), icon()]
 });
